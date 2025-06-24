7573565edbe65fe9000b1c0989b6e40da93bd563
Melker Widén <melker.widen@gmail.com>
2025-06-24 14:04:28 +0200

    Add keybind for diagnostics

--- core/.config/nvim/lua/memagu/remap.lua
+++ core/.config/nvim/lua/memagu/remap.lua
@@ -23,3 +23,5 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<left><left><left>")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true } )
+
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float)
