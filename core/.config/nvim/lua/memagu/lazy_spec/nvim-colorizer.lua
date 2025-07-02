9981848317c171f18edf7de515f93accdd51e455
Melker Widén <melker.widen@gmail.com>
2025-07-02 15:36:01 +0200

    Add nvim-colorizer

---
+++ core/.config/nvim/lua/memagu/lazy_spec/nvim-colorizer.lua
@@ -0,0 +1,15 @@
+return {
+    "catgoose/nvim-colorizer.lua",
+    opts = {
+        user_default_options = {
+            names_opts = { uppercase = true },
+            css = true,
+            tailwind = true,
+            tailwind_opts = { update_names = true },
+            sass = { enable = true, parsers = { "css" } },
+            mode = "virtualtext",
+            always_update = true,
+        }
+    }
+}
+
