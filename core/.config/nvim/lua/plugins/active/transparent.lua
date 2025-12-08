0030bf29b5c603b61a09bb28becc49fc20d8317e
Melker Widén <melker.widen@gmail.com>
2025-12-08 15:34:10 +0100

    Semi fix transparent

--- core/.config/nvim/lua/plugins/active/transparent.lua
+++ core/.config/nvim/lua/plugins/active/transparent.lua
@@ -6,49 +6,12 @@ return {
         local transparent = require("transparent")
 
         transparent.setup({
-            groups = {
-                "Normal",
-                "NormalNC",
-                "NormalFloat",
-                "Comment",
-                "Constant",
-                "Special",
-                "Identifier",
-                "Statement",
-                "PreProc",
-                "Type",
-                "Underlined",
-                "Todo",
-                "String",
-                "Function",
-                "Conditional",
-                "Repeat",
-                "Operator",
-                "Structure",
-                "LineNr",
-                "NonText",
-                "SignColumn",
-                "CursorLine",
-                "CursorLineNr",
-                "StatusLine",
-                "StatusLineNC",
-                "EndOfBuffer",
-                "DiagnosticVirtualTextError",
-                "DiagnosticVirtualTextWarn",
-                "DiagnosticVirtualTextInfo",
-                "DiagnosticVirtualTextHint",
-                "DiagnosticSignError",
-                "DiagnosticSignWarn",
-                "DiagnosticSignInfo",
-                "DiagnosticSignHint",
-                "DiagnosticUnderlineError",
-                "DiagnosticUnderlineWarn",
-                "DiagnosticUnderlineInfo",
-                "DiagnosticUnderlineHint"
-            },
-            extra_groups = {},
-            exclude_groups = {}
+            -- extra_groups = {},
+            exclude_groups = {
+                "NormalFloat"
+            }
         })
+
         transparent.toggle(true)
     end
 }
