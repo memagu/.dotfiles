d355abae337c269e5017d4f94fa9dd379feb79e3
Melker Widén <melker.widen@gmail.com>
2025-12-08 15:33:41 +0100

    Fix markdown preview building issues

--- core/.config/nvim/lua/plugins/active/markdown-preview.lua
+++ core/.config/nvim/lua/plugins/active/markdown-preview.lua
@@ -1,6 +1,10 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = "cd app && npm install",
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
 }
+
