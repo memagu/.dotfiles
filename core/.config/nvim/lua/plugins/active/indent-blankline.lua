1ea1b6d9ca7dfa5d2f19ccd0a02db943bbdcf3c3
Melker Widén <melker.widen@gmail.com>
2026-01-14 00:19:57 +0100

    Remove annoying underlines

--- core/.config/nvim/lua/plugins/active/indent-blankline.lua
+++ core/.config/nvim/lua/plugins/active/indent-blankline.lua
@@ -3,13 +3,16 @@ return {
     main = "ibl",
     ---@module "ibl"
     ---@type ibl.config
-    opts = {},
+    opts = { scope = {
+        show_start = false,
+        show_end = false
+    } },
     config = function(_, opts)
         -- ensure highlights exist after colorscheme overrides
         local hooks = require("ibl.hooks")
         hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
             vim.api.nvim_set_hl(0, "IblScope",  { fg = "#f280a1", bold = true })
-            vim.api.nvim_set_hl(0, "IblIndent", { fg = "#9966cc" })
+            vim.api.nvim_set_hl(0, "IblIndent", { fg = "#9966cc", bold = false })
         end)
 
         require("ibl").setup(opts)
