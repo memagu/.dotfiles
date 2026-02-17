35dd1d6ff91f20a8e151ac59272f75049db8cb08
Melker Widén <melker.widen@gmail.com>
2026-02-17 12:37:39 +0100

    Improve sets

--- config/core/.config/nvim/lua/config/set.lua
+++ config/core/.config/nvim/lua/config/set.lua
@@ -1,7 +1,9 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -9,12 +11,19 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.wrap = false
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "↪ "
+
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 16
+
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "120"
 
 vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
