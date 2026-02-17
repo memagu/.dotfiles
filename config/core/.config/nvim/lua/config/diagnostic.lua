671c8c5b3c45a4ca9f0996ec84b4dfca282a5ed7
Melker Widén <melker.widen@gmail.com>
2026-02-18 00:35:45 +0100

    Simplify diagnostic config

--- config/core/.config/nvim/lua/config/diagnostic.lua
+++ config/core/.config/nvim/lua/config/diagnostic.lua
@@ -1,18 +1,15 @@
-local function fmt_diagnostic(diagnostic) return diagnostic.message end
-
 vim.diagnostic.config({
   severity_sort = true,
+  update_in_insert = true,
   float = { border = "rounded", style = "minimal", source = "if_many", header = "", prefix = "" },
-  underline = true,
+  virtual_lines = { current_line = true },
+  virtual_text = { source = "if_many", spacing = 4 },
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = " ",
-      [vim.diagnostic.severity.WARN] = " ",
-      [vim.diagnostic.severity.INFO] = " ",
-      [vim.diagnostic.severity.HINT] = "󰌵 ",
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "󰌵",
     },
   },
-  virtual_lines = { current_line = true, format = fmt_diagnostic },
-  update_in_insert = true,
-  virtual_text = { source = "if_many", spacing = 4, format = fmt_diagnostic },
 })
