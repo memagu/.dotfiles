9d9ba8e1445a65df2c7d67ead343740545d96257
Melker Widén <melker.widen@gmail.com>
2026-07-11 19:08:26 +0200

    Update blink-pairs

--- config/core/.config/nvim/lua/plugins/active/blink-pairs.lua
+++ config/core/.config/nvim/lua/plugins/active/blink-pairs.lua
@@ -1,11 +1,15 @@
 return {
   "saghen/blink.pairs",
   version = "*",
-  dependencies = "saghen/blink.download",
-  event = "BufReadPost",
-  opts = {
-    mappings = {
-      enabled = false,
-    },
-  },
+  dependencies = "saghen/blink.lib",
+  build = function() require("blink.pairs").download():pwait(60000) end,
+  lazy = false,
+  config = function()
+    require("vim._core.ui2").enable({})
+    require("blink.pairs").setup({
+      mappings = {
+        enabled = false,
+      },
+    })
+  end,
 }
